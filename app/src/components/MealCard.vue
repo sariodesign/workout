91b2542c246b3db87fbcd6ec7fad3340cdec3754
Ros <ros@iMac-di-ciaopeople.local>
2022-01-19 18:43:43 +0100

    Update workout

---
+++ app/src/components/MealCard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="card">
+      <div class="card-tag"></div>
+      <div class="card-text"></div>
+    </div>
+</template>
+
+<script>
+export default {
+  props: {
+    meal: {
+      type: Object,
+      required: true
+    }
+  }
+};
+</script>
+
+<style scoped>
+
+</style>
---
+++ app/src/views/Nutrition.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="nutrition">
+    <MealCard/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import MealCard from "@/components/MealCard.vue";
+import axios from 'axios';
+
+export default {
+  name: "nutrition",
+  components: {
+    MealCard,
+  },
+  data() {
+    return {
+      meals: null
+    }
+  },
+  created() {
+    const weekday = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = new Date();
+    let today = weekday[day.getDay()].toLowerCase();
+    console.log(today);
+    axios.get('http://my-json-server.typicode.com/sariodesign/workout/nutrition')
+      .then(response => {
+        //this.meals = response.data
+        console.log('[LOAD RESPONSE]')
+        console.log(response.data);
+      })
+      .catch(error => {
+        console.log('[ERROR]', error)
+      })
+  }
+};
+</script>
