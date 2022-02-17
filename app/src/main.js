a39fed87307ab49a51aec7148c16b9c9a019109d
Ros <ros@iMac-di-ciaopeople.local>
2022-02-17 16:11:11 +0100

    Update mainjs vue app

--- app/src/main.js
+++ app/src/main.js
@@ -4,8 +4,3 @@ import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
-
-// Service Worker
-if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('./sw.js');
-};
