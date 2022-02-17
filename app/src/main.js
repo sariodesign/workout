73233090c8a41d14dacf3978fbe6e6f01749fce7
Ros <ros@iMac-di-ciaopeople.local>
2022-02-17 15:55:55 +0100

    Added service worker

--- app/src/main.js
+++ app/src/main.js
@@ -4,3 +4,8 @@ import router from "./router";
 import store from "./store";
 
 createApp(App).use(store).use(router).mount("#app");
+
+// Service Worker
+if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('./sw.js');
+};
