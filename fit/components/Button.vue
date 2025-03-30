7ed9c6833fa1de2005c595e28978be319bd7429d
Rosario <sariodesign.it@gmail.com>
2025-03-30 19:27:24 +0200

    Completed welcome page (#8)
    * feat: Init project with Welcome page (#1)
    
    * completed welcome page

---
+++ fit/components/Button.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+import { defineProps } from 'vue'
+
+interface ButtonProps {
+  link: string
+  text: string
+}
+const props = defineProps<ButtonProps>()
+</script>
+
+<template>
+  <NuxtLink :to="props.link" class="bg-accent-primary text-white font-heading rounded-4xl uppercase leading-none text-xl text-center px-4 py-2">{{props.text}}</NuxtLink>
+</template>
